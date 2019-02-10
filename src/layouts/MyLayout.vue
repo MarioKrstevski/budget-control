ca65beeb3c1746e9b7688d220c330d4f2158346d
Mario Krstevski <mariokrstevski@hotmail.com>
2019-02-10 02:39:44 +0100

    [Sidebar] Tweaking the sidebar settings

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -3,13 +3,11 @@
     <q-layout-header>
       <q-toolbar
         color="primary"
-        :glossy="$q.theme === 'mat'"
         :inverted="$q.theme === 'ios'"
       >
         <q-btn
           flat
           dense
-          round
           @click="leftDrawerOpen = !leftDrawerOpen"
           aria-label="Menu"
         >
@@ -17,7 +15,7 @@
         </q-btn>
 
         <q-toolbar-title>
-          Quasar App
+          Budget Control
           <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
         </q-toolbar-title>
       </q-toolbar>
@@ -26,36 +24,51 @@
     <q-layout-drawer
       v-model="leftDrawerOpen"
       :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+      class="justify-center"
     >
+      <div style="display:grid; alignItems:center; height:100%">
+        <q-list
+        no-border
+        sparse
+        link
+      >
+        <q-list-header class="blue-grey-10">Previews</q-list-header>
+        <q-item>
+          <q-item-side icon="school" />
+          <q-item-main label="Dashboard"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="code" />
+          <q-item-main label="Transactions"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="chat" />
+          <q-item-main label="Budgets"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="record_voice_over" />
+          <q-item-main label="Statistics"/>
+        </q-item>
+      </q-list>
+
       <q-list
         no-border
         link
+        sparse
         inset-delimiter
       >
-        <q-list-header>Essential Links</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
+        <q-list-header>Account</q-list-header>
+        <q-item>
           <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+          <q-item-main label="Settings"/>
         </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
+        <q-item>
           <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
+          <q-item-main label="Sign Out"/>
         </q-item>
       </q-list>
+      </div>
     </q-layout-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
